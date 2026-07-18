43a02862247a3aee70daf6cdfb7c7ef74460eadf
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-07-18 17:05:05 +0000

    0.0.12 test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -50,7 +50,7 @@ export default defineConfig([
        "prefer-const":
          "error" },
    files:
-     [ "**/*js,mjs,cjs}",
+     [ "**/*.{js,mjs,cjs}",
        `**/${_project}*`
      ],
    plugins:
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,25 +1,29 @@
-/**    ----------------------------------------------------------------
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/**    ---------------------------------------------------------
  *     webpack.config.cjs
- *     ----------------------------------------------------------------
+ *     ---------------------------------------------------------
  *     Copyright ©
  *       Pellegrino Prevete
  *         2024, 2025, 2026
  * 
  *     All rights reserved
- *     ----------------------------------------------------------------
+ *     ---------------------------------------------------------
  * 
  *     This program is free software: you can redistribute it and/or
- *     modify it under the terms of the GNU General Public License as
- *     published by the Free Software Foundation, either version 3 of
- *     the License, or (at your option) any later version.
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either
+ *     version 3 of the License, or (at your option) any later
+ *     version.
  * 
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     This program is distributed in the hope that it will be
+ *     useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE.
+ *     See the GNU General Public License for more details.
  * 
- *     You should have received a copy of the GNU General Public License
- *     along with this program.
+ *     You should have received a copy of the
+ *     GNU General Public License along with this program.
  *     If not, see <https://www.gnu.org/licenses/>.
  */
 
@@ -48,13 +52,9 @@ const
       _output_file_name
 };
 const
-  _utils_ignore =
+  _node_fs_ignore =
   { resourceRegExp:
-      /^utils$/ };
-const
-  _web_worker_ignore =
-  { resourceRegExp:
-      /^web-worker$/ };
+      /^node:fs$/ };
 const
   _yargs_ignore =
   { resourceRegExp:
@@ -71,13 +71,9 @@ const
   _ignore_plugin =
     _webpack.IgnorePlugin; 
 const
-  _utils_ignore_plugin =
+  _node_fs_ignore_plugin =
     new _ignore_plugin(
-          _utils_ignore);
-const
-  _web_worker_ignore_plugin =
-    new _ignore_plugin(
-          _web_worker_ignore);
+          _node_fs_ignore);
 const
   _yargs_ignore_plugin =
     new _ignore_plugin(
@@ -96,14 +92,18 @@ module.exports = {
   },
   resolve: {
     alias: {
+      "node:fs":
+        _path.resolve(
+          __dirname,
+          'node_modules/opfs/opfs'),
       "evm-transactions-tools":
         _path.resolve(
           __dirname,
-          "libevm-transactions-tools"),
+          "evm-transactions-tools"),
       "fs":
         _path.resolve(
           __dirname,
-          'node_modules/fs/fs'),
+          "node_modules/fs/fs"),
       "path":
         _path.resolve(
           __dirname,
@@ -140,6 +140,7 @@ module.exports = {
     { yargs:
         'yargs' },
   plugins: [
+    _node_fs_ignore_plugin,
     _yargs_ignore_plugin,
     _yargs_helpers_ignore_plugin
   ]
